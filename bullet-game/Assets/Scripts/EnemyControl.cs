cd5b39a961890049cd6e5bf54e67b1de0b353f45
Spencer <sburke17@emich.edu>
2020-11-07 21:26:43 -0500

    Added Bullets, Bullet destroyer, Colliders to ships

---
+++ bullet-game/Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    public bool killLoop = false;
+    public float shotDelay = 1.0f;
+    public GameObject bullet;
+    public Transform bulletSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Run());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Run()
+    {
+        GameObject tempBullet;
+        while (!killLoop)
+        {
+            yield return new WaitForSeconds(shotDelay);
+            tempBullet = Instantiate(bullet);
+            tempBullet.transform.position = bulletSpawn.position;
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entred Trigger");
+        if(other.tag == "PlayerBullet")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
