5bf32484e45679ddffca8a393c3d14209462b681
Spencer <sburke17@emich.edu>
2020-11-12 13:36:22 -0500

    Add player bullets

--- bullet-game/Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ bullet-game/Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public float shotDelay = .5f;
+    public GameObject bullet;
+    public Transform bulletSpawn;
+    private GameObject tempBullet;
+    private bool canFire = true;
+    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,20 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!canFire && timer >0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+                canFire = true;
+        }
+        if (Input.GetButtonDown("Fire1") && canFire)
+        {
+            tempBullet = Instantiate(bullet);
+            tempBullet.transform.position = bulletSpawn.position;
+            tempBullet.transform.rotation = bulletSpawn.rotation;
+            canFire = false;
+            timer = shotDelay;
+        }
         
     }
 }
