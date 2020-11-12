a31b42b9eb2e2907710433fe32922fd39a1c37a2
Spencer <sburke17@emich.edu>
2020-11-12 13:25:32 -0500

    Player shield regens, added color change on damage

--- bullet-game/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ bullet-game/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     public Slider healthBar;
-    public int maxHealth;
-    public int currentHealth;
-    public bool shielding = false;
+    public float maxHealth;
+    private float currentHealth;
+    private bool shielding = false;
     public Slider shieldBar;
-    public int maxShield;
-    public int currentShield;
+    public float maxShield;
+    private float currentShield;
     public GameObject shield;
+
+    private Renderer shieldRend;
+    private Color startColor;
+    public Color brokenColor;
+    public float shieldRegenDelay = .5f;
+    private float timer;
+    public float shieldRegenSpeed = .05f;
+    private bool regenerating = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +31,9 @@ public class PlayerHealth : MonoBehaviour
         shieldBar.maxValue = maxShield;
         shieldBar.value = maxShield;
         currentShield = maxShield;
+
+        shieldRend = shield.GetComponent<Renderer>();
+        startColor = shieldRend.material.color;
         shield.SetActive(false);
     }
 
@@ -50,23 +61,51 @@ public class PlayerHealth : MonoBehaviour
             shielding = false;
             shield.SetActive(false);
         }
+        //If the timer is not 0, start counting down
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            //If the timer is over, set the regen flag
+            if(timer <= 0)
+            {
+                regenerating = true;
+            }
+        }
+        if (regenerating) //If the regen flag is set, regen
+        {
+            currentShield += shieldRegenSpeed * Time.deltaTime;
+            //Reflect the change in the shield bar and the shield color
+            shieldRend.material.color = Color.Lerp(brokenColor, startColor, currentShield / maxShield);
+            shieldBar.value = currentShield;
+            //Is the shield full? if so, stop regenerating
+            if (currentShield > maxShield)
+            {
+                regenerating = false;
+                currentShield = maxShield;
+            }
+        }
     }
     void DamageShield(int damage)
     {
+        //You took shield damage, reset the timer and make sure the regen flag is off
+        timer = shieldRegenDelay;
+        regenerating = false;
+
         currentShield -= damage;
         if(currentShield < 0)
         {
-            int damageLeft = Mathf.Abs(currentShield);
+            float damageLeft = Mathf.Abs(currentShield);
             currentShield = 0;
             shieldBar.value = 0;
             DamageHealth(damageLeft);
         }
         else
         {
+            shieldRend.material.color = Color.Lerp(brokenColor, startColor, currentShield / maxShield);
             shieldBar.value = currentShield;
         }
     }
-    void DamageHealth(int damage)
+    void DamageHealth(float damage)
     {
         currentHealth -= damage;
         healthBar.value = currentHealth;
