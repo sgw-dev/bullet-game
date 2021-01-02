ced9e7833faf43527196da62e8dbb81bf380f706
Spencer <sburke17@emich.edu>
2021-01-02 13:30:19 -0500

    Changed Input scheme, added pause feature, messed with scene loading

--- bullet-game/Assets/Scripts/GameStateManager.cs
+++ bullet-game/Assets/Scripts/GameStateManager.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public enum GameState
 {
-    Default, StartMenu, InLevel, Options
+    Default, StartMenu, InLevel, Options, Paused
 }
 public class GameStateManager : MonoBehaviour
 {
     private GameState state = GameState.Default;
+    private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
+        cam = Camera.main;
         if(state == GameState.Default)
         {
             LoadMap("StartMenu");
@@ -32,6 +35,8 @@ public class GameStateManager : MonoBehaviour
         // Does a single mode load of the map scene
         AsyncOperation op = SceneManager.UnloadSceneAsync("StartMenu");
         //op.completed += this.UnloadLoadingScreen; // Unloads loading screen on map load
+        //Remove Camera and Light from scene
+        cam.enabled = false;
     }
     public void NewGame()
     {
@@ -66,5 +71,21 @@ public class GameStateManager : MonoBehaviour
     private void UnloadLoadingScreen(AsyncOperation op = null)
     {
         SceneManager.UnloadSceneAsync("LoadingScreen");
+    }
+
+    //UI Functions
+    public void Pause(InputAction.CallbackContext context)
+    {
+        if(state != GameState.Paused)
+        {
+            state = GameState.Paused;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            state = GameState.InLevel;
+            Time.timeScale = 1;
+        }
+        
     }
 }
